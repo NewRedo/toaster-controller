53d3d6d29f591676280d5edd34e46115604369f8
Alan Raison <alan.raison@newredo.com>
2023-05-02 14:47:44 +0000

    3-controllers result

--- breakfast/internal/controller/toast_controller.go
+++ breakfast/internal/controller/toast_controller.go
@@ -18,7 +18,9 @@ package controller
 
 import (
 	"context"
+	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -26,6 +28,8 @@ import (
 
 	breakfastv1 "github.com/NewRedo/toaster-controller/breakfast/api/v1"
 )
+
+const TOAST_INTERVAL = 10 * time.Second
 
 // ToastReconciler reconciles a Toast object
 type ToastReconciler struct {
@@ -47,10 +51,48 @@ type ToastReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *ToastReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
 	// TODO(user): your logic here
+	toast := &breakfastv1.Toast{}
+	err := r.Get(ctx, req.NamespacedName, toast)
 
+	if err != nil && client.IgnoreNotFound(err) == nil {
+		logger.Info("Toast not found, assumed deleted")
+		return ctrl.Result{}, nil
+	}
+	if client.IgnoreNotFound(err) != nil {
+		logger.Error(err, "unable to fetch Toast")
+		return ctrl.Result{}, err
+	}
+
+	if time.Now().Before(toast.Status.LastUpdated.Add(TOAST_INTERVAL)) {
+		return ctrl.Result{}, nil
+	}
+
+	if toast.Status.Toastiness < toast.Spec.Toastiness {
+		logger.Info("status toastiness < spec toastiness")
+		toast.Status.Toastiness++
+		toast.Status.Ready = false
+		toast.Status.LastUpdated = metav1.Now()
+
+		if err = r.Status().Update(ctx, toast); err != nil {
+			logger.Error(err, "unable to update Toast")
+			return ctrl.Result{}, err
+		}
+
+		return ctrl.Result{
+			RequeueAfter: TOAST_INTERVAL,
+		}, nil
+	}
+
+	// Update the status after the toastiness reaches the desired level
+	toast.Status.Ready = true
+	if err = r.Status().Update(ctx, toast); err != nil {
+		logger.Error(err, "unable to update Toast")
+		return ctrl.Result{}, err
+	}
+	// Don't requeue if the toast is ready
 	return ctrl.Result{}, nil
 }
 
